3d12d32dab1706c2e60a73a184dc6384a2f4b2c2
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2025-05-22 00:38:10 -0300

    feat(Map page): renderização do mapa, implementação da navbar, e começo dos componentes de input e cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { MapComponent } from './pages/map/map.component';
 
 export const routes: Routes = [
     {
@@ -29,6 +28,6 @@ export const routes: Routes = [
     },
     {
         path: 'map',
-        component: MapComponent
+        loadComponent: () => import('./pages/map/map.component')
     }
 ];
--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
+  imports: [NavbarComponent],
 })
-export class MapComponent implements OnInit {
+export default class MapComponent implements OnInit {
   map: any;
 
   ngOnInit(): void {
@@ -14,9 +16,15 @@ export class MapComponent implements OnInit {
   }
 
   private initMap(): void {
+    const bounds = L.latLngBounds(L.latLng(-1.45, -48.48), L.latLng(-1.45, -48.48));
+
     this.map = L.map('map', {
-      center: [51.505, -0.09],
+      center: [-1.4558, -48.4902],
       zoom: 13,
+      minZoom: 13,
+      maxZoom: 15,
+      maxBounds: bounds,
+      maxBoundsViscosity: 2.0,
     });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
