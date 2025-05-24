9de7c4408cf786d925124d8691abd51362e2ff0e
unknown <matheusleao585@gmail.com>
2025-05-24 15:25:53 -0300

    fix: estilização

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { SearchBarComponent } from "../../components/search-bar/search-bar.compo
 import { NgFor } from '@angular/common';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { ForYouComponent } from "../../components/for-you/for-you.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [SearchBarComponent, NgFor, NavbarComponent, ForYouComponent],
+  imports: [SearchBarComponent, NgFor, NavbarComponent, ForYouComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -19,21 +20,24 @@ export default class HomeComponent {
     tipo: 'Feira',
     local: 'Belém, Pará',
     imagem: 'assets/veroPeso.png',
-    nota: 4.8
+    nota: 4.8,
+    rota:'/market'
   },
   {
     nome: 'Mercado de São Brás',
     tipo: 'Feira',
     local: 'Belém, Pará',
     imagem: 'assets/saoBras.jpg',
-    nota: 4.6
+    nota: 4.6,
+    rota:'/market'
   },
   {
     nome: 'Comércio',
     tipo: 'Feira',
     local: 'Belém, Pará',
     imagem: 'assets/comercio.jpg',
-    nota: 4.6
+    nota: 4.6,
+    rota:'/market'
   },
   
 ];
--- src/app/pages/market-info/market-info.component.ts
+++ src/app/pages/market-info/market-info.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-market-info',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './market-info.component.html',
   styleUrl: './market-info.component.scss'
 })
