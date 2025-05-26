c093eb136c254590168dba41f23a96f4ee82f16b
unknown <matheusleao585@gmail.com>
2025-05-26 00:01:30 -0300

    feat: tela de perfil do usuario

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { MainButtonComponent } from "../../components/main-button/main-button.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MainButtonComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
