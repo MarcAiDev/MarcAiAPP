ad416e6ecb79a33d14c472d551f344a833d896af
unknown <matheusleao585@gmail.com>
2025-05-26 01:45:48 -0300

    fix: modal de sucesso no cadastro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { MainInputComponent } from "../../components/main-input/main-input.component";
 import { MainButtonComponent } from '../../components/main-button/main-button.component';
 import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [MainInputComponent, MainButtonComponent, RouterLink],
+  imports: [MainInputComponent, MainButtonComponent, RouterLink,NgIf, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export default class RegisterComponent {
+  showSuccessModal = false;
 
+    onRegister() {
+      // Aqui você colocaria a lógica de cadastro...
+
+      // Depois mostra o modal
+      this.showSuccessModal = true;
+    }
+
+    closeModal() {
+      this.showSuccessModal = false;
+    }
 }
